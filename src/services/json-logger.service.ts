1bd2cc4ad8fc2209810a2dd2f1b652e3fc6570bc
ikeizak <i.kazak@mobilexapps.com>
2025-05-27 16:29:35 +0300

    update logger

--- src/services/json-logger.service.ts
+++ src/services/json-logger.service.ts
@@ -1,7 +1,5 @@
 import { LoggerService } from "@nestjs/common";
 import { hostname } from "os";
-import * as fs from "fs";
-import * as path from "path";
 
 export enum LogLevels {
     ФАТАЛЬНАЯ = 60,
@@ -25,48 +23,22 @@ function levelToRussian(level: number): string {
 }
 
 export class JsonLogger implements LoggerService {
-    protected hostname: string = hostname();
-    private logStream: fs.WriteStream;
-
-    constructor() {
-        const logDir = path.resolve(process.cwd(), "logs");
-        if (!fs.existsSync(logDir)) {
-            fs.mkdirSync(logDir, { recursive: true });
-        }
-        this.logStream = fs.createWriteStream(path.join(logDir, "botview.log"), { flags: "a" });
-    }
-
-    /**
-     * Write a 'log' level log.
-     */
     public log(message: any) {
         this.writeJson(message, LogLevels.ИНФО);
     }
 
-    /**
-     * Write an 'error' level log.
-     */
     public error(message: any) {
         this.writeJson(message, LogLevels.ОШИБКА);
     }
 
-    /**
-     * Write a 'warn' level log.
-     */
     public warn(message: any) {
         this.writeJson(message, LogLevels.ПРЕДУПРЕЖДЕНИЕ);
     }
 
-    /**
-     * Write a 'debug' level log.
-     */
     public debug(message: any) {
         this.writeJson(message, LogLevels.ОТЛАДКА);
     }
 
-    /**
-     * Write a 'verbose' level log.
-     */
     public verbose(message: any) {
         this.writeJson(message, LogLevels.ПОДРОБНО);
     }
@@ -96,6 +68,5 @@ export class JsonLogger implements LoggerService {
         }
         const logStr = JSON.stringify(logObj, null, 2);
         console.log(logStr);
-        this.logStream.write(logStr + "\n");
     }
 }
